58fd9f16719199335f0f043e02d28c3356cd8d46
neddington <neddington@cascadia.edu>
2023-12-11 16:24:36 -0800

    Initial Commit

---
+++ UsingMachineLearning/UsingMachineLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingMachineLearningApp.swift
+//  UsingMachineLearning
+//
+//  Created by Eddington, Nick on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct UsingMachineLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
